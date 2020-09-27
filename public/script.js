6ea30e9a7303a61d927eaff769ee485de1b5c178
Brooks Benson <brooksbenson03@gmail.com>
2020-09-27 17:46:43 -0600

    public/scripts.js: polished

--- public/script.js
+++ public/script.js
@@ -1,53 +1,47 @@
-class DomHelper {
-  setAttributes(element, attributes) {
-    for (let a in attributes) {
-      element.setAttribute(a, attributes[a])
-    }
-  }
-}
-
-class Photo extends DomHelper {
-  constructor(photoData, container) {
-    super()
+class Photo {
+  constructor(photoData, photoContainer) {
     this.photoData = photoData
-    this.container = container
-    this.linkElement = document.createElement('a')
-    this.imageElement = document.createElement('img')
+    this.photoElement = this.createPhotoElement(photoData)
+    this.photoContainer = photoContainer
+  }
 
-    this.setAttributes(this.linkElement, {
-      href: photoData.links.html,
-      target: "_blank",
-    })
+  createPhotoElement(photoData) {
+    const linkElement = document.createElement('a')
+    linkElement.setAttribute('href', photoData.links.html)
+    linkElement.setAttribute('target', '_blank')
 
-    this.setAttributes(this.imageElement, {
-      src: photoData.urls.regular,
-      alt: photoData.alt_description,
-      title: photoData.alt_description,
-    })
+    const imageElement = document.createElement('img')
+    imageElement.setAttribute('src', photoData.urls.regular)
+    imageElement.setAttribute('alt', photoData.alt_description)
+    imageElement.setAttribute('title', photoData.alt_description)
+
+    linkElement.append(imageElement)
+
+    return linkElement
   }
 
   render() {
-    this.linkElement.append(this.imageElement)
-    this.container.appendChild(this.linkElement)
+    this.photoContainer.appendChild(this.photoElement)
   }
 }
 
 class Photos {
   constructor() {
+    this.isAddingPhotos = false
     this.loader = document.getElementById('loader')
     this.photoContainer = document.getElementById("image-container")
-    this.isAddingPhotos = false
-    this.unhandledPhotoData = []
+    this.unrenderedPhotos = []
     this.renderedPhotos = []
     this.addPhotos = this.addPhotos.bind(this)
     this.infiniteScroll = this.infiniteScroll.bind(this)
   }
 
-  async fetchPhotoData() {
+  async getNewPhotos() {
     const response = await fetch(
       "https://api.unsplash.com/photos/random/?client_id=wd_tiI1oTG1RUOBwojwOl-rkz7X9M6o_hpPE-WySCi0&count=10"
     )
-    return await response.json()
+    this.unrenderedPhotos = await response.json()
+    this.unrenderedPhotos = this.unrenderedPhotos.map(data => new Photo(data, this.photoContainer))
   }
 
   async addPhotos() {
@@ -56,12 +50,11 @@ class Photos {
     } else {
       this.isAddingPhotos = true
       this.loader.hidden = false
-      this.unhandledPhotoData = await this.fetchPhotoData()
-      this.unhandledPhotoData = this.unhandledPhotoData.map(data => new Photo(data, this.photoContainer))
-      while (this.unhandledPhotoData.length) {
-        this.unhandledPhotoData[0].render()
-        this.renderedPhotos.push(this.unhandledPhotoData[0])
-        this.unhandledPhotoData.shift()
+      await this.getNewPhotos()
+      while (this.unrenderedPhotos.length) {
+        this.unrenderedPhotos[0].render()
+        this.renderedPhotos.push(this.unrenderedPhotos[0])
+        this.unrenderedPhotos.shift()
       }
       this.isAddingPhotos = false
       this.loader.hidden = true
