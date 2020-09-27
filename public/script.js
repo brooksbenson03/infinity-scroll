41b47fa1514d97e08fafe1cd1fadda57de0cf012
Brooks Benson <brooksbenson03@gmail.com>
2020-09-27 00:52:29 -0600

    public/script.js: created DomHelper, Photo, Photos classes

--- public/script.js
+++ public/script.js
@@ -1,57 +1,82 @@
-const imageContainer = document.getElementById("image-container")
-const loader = document.getElementById("loader")
+class DomHelper {
+  setAttributes(element, attributes) {
+    for (let a in attributes) {
+      element.setAttribute(a, attributes[a])
+    }
+  }
+}
 
-function setAttrs(el, attrs) {
-  for (let attr in attrs) {
-    el.setAttribute(attr, attrs[attr])
+class Photo extends DomHelper {
+  constructor(photoData, container) {
+    super()
+    this.photoData = photoData
+    this.container = container
+    this.linkElement = document.createElement('a')
+    this.imageElement = document.createElement('img')
+
+    this.setAttributes(this.linkElement, {
+      href: photoData.links.html,
+      target: "_blank",
+    })
+
+    this.setAttributes(this.imageElement, {
+      src: photoData.urls.regular,
+      alt: photoData.alt_description,
+      title: photoData.alt_description,
+    })
+  }
+
+  render() {
+    this.linkElement.append(this.imageElement)
+    this.container.appendChild(this.linkElement)
   }
 }
 
 class Photos {
   constructor() {
+    this.loader = document.getElementById('loader')
+    this.photoContainer = document.getElementById("image-container")
     this.isAddingPhotos = false
-    this.photos = []
+    this.unhandledPhotoData = []
+    this.renderedPhotos = []
+    this.addPhotos = this.addPhotos.bind(this)
+    this.infiniteScroll = this.infiniteScroll.bind(this)
   }
 
-  async addPhotos() {
-    if (this.isAddingPhotos) return
-    this.isAddingPhotos = true
+  async fetchPhotoData() {
     const response = await fetch(
       "https://api.unsplash.com/photos/random/?client_id=wd_tiI1oTG1RUOBwojwOl-rkz7X9M6o_hpPE-WySCi0&count=10"
     )
-    const photos = await response.json()
-    this.photos = [...this.photos, ...photos]
-    this.displayPhotos()
-    this.isAddingPhotos = false
+    return await response.json()
   }
 
-  displayPhotos() {
-    this.photos.forEach((p) => {
-      const item = document.createElement("a")
-      setAttrs(item, {
-        href: p.links.html,
-        target: "_blank",
-      })
-      const img = document.createElement("img")
-      setAttrs(img, {
-        src: p.urls.regular,
-        alt: p.alt_description,
-        title: p.alt_description,
-      })
-      item.append(img)
-      imageContainer.appendChild(item)
-    })
+  async addPhotos() {
+    if (this.isAddingPhotos) {
+      return
+    } else {
+      this.isAddingPhotos = true
+      this.loader.hidden = false
+      this.unhandledPhotoData = await this.fetchPhotoData()
+      this.unhandledPhotoData = this.unhandledPhotoData.map(data => new Photo(data, this.photoContainer))
+      while (this.unhandledPhotoData.length) {
+        this.unhandledPhotoData[0].render()
+        this.renderedPhotos.push(this.unhandledPhotoData[0])
+        this.unhandledPhotoData.shift()
+      }
+      this.isAddingPhotos = false
+      this.loader.hidden = true
+    }
+  }
+
+  infiniteScroll() {
+    const scrolledAmount = window.innerHeight + window.scrollY
+    const fetchPhotosTriggerPoint = document.body.offsetHeight - 1000
+  
+    if (scrolledAmount > fetchPhotosTriggerPoint) this.addPhotos()
   }
 }
 
 const photos = new Photos()
 
-window.addEventListener("scroll", () => {
-  const scrolledAmount = window.innerHeight + window.scrollY
-  const fetchPhotosTriggerPoint = document.body.offsetHeight - 1000
-
-  if (scrolledAmount > fetchPhotosTriggerPoint) photos.addPhotos()
-})
-
-// on load
-photos.addPhotos()
+window.addEventListener('DOMContentLoaded', photos.addPhotos)
+window.addEventListener('scroll', photos.infiniteScroll)
